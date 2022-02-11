77389d61615904d7778a20f499e44d8ba0f9f8e7
saiashish9 <saiashish3760@gmail.com>
2022-02-11 19:53:29 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sai Ashish Darapureddy on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
